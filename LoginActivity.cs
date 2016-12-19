70269005f69e0fb476765f3d9137f2e55e5f2f1a
Llewellyn <Llewellyn@DESKTOP-FM6CJGQ>
2016-12-19 23:58:02 +1100

    - Created a Login layout and activity

---
+++ LoginActivity.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DragAndDropDemo
+{
+    [Activity(Label = "LoginActivity")]
+    public class LoginActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            // Set the login layout.
+            SetContentView(Resource.Layout.Login);
+        }
+    }
+}
