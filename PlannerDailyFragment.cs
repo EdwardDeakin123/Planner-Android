24e8ec4d4b11da9cb2d598cc4c1828d282ed7277
Llewellyn <Llewellyn@DESKTOP-FM6CJGQ>
2017-01-21 21:07:09 +1100

    - Converted the planner activities to fragments - Added a navigation drawer to the main activity - Added the Daily fragment as the default (still need to implement switching between types).

---
+++ PlannerDailyFragment.cs
@@ -0,0 +1,96 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Front_End.Models;
+using Front_End.Backend;
+using Front_End.Exceptions;
+using SQLite;
+using Front_End.Database;
+
+namespace Front_End
+{
+    [Activity(Label = "Activity Tracker")]
+	public class PlannerDailyFragment : PlannerFragment
+	{
+		public override void OnCreate(Bundle bundle)
+		{
+            base.OnCreate(bundle);
+
+            if(_ViewDate == default(DateTime))
+            {
+                DateTime currentTime = DateTime.Now;
+
+                // If the current view date is not set, set it to the current day.
+                _ViewDate = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, 0, 0, 0);
+            }
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Return the layout to use in this fragment.
+            return inflater.Inflate(Resource.Layout.PlannerDaily, container, false);
+        }
+
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            base.OnActivityCreated(savedInstanceState);
+
+            // The view has been created, assign the event handlers.
+            // Assign the next and previous onclick events.
+            View.FindViewById<ImageButton>(Resource.Id.ibNextDate).Click += NextDay_OnClick;
+            View.FindViewById<ImageButton>(Resource.Id.ibPrevDate).Click += PreviousDay_OnClick;
+
+            // Get the dropzone and attach a drag event listener.
+            View.FindViewById<RelativeLayout>(Resource.Id.rlDropzone).Drag += DropZone_Drag;
+        }
+
+        public override void OnResume()
+        {
+            base.OnResume();
+
+            // Reload the planner when the activity is resumed.
+            ReloadUI();
+
+            TimesDatabase timeDb = new TimesDatabase();
+
+            if (timeDb.FindLatest() != DateTime.Now.Day)
+            {
+                new Notification();
+            }
+        }
+	
+        #region backend
+        #endregion
+
+        #region event handlers
+        // TODO: Make these abstract, rename to Next and Previous 
+        private void PreviousDay_OnClick(object sender, EventArgs e)
+        {
+            // Update the view date
+            _ViewDate = _ViewDate.AddDays(-1);
+
+            // Reload the UI.
+            ReloadUI();
+        }
+
+        private void NextDay_OnClick(object sender, EventArgs e)
+        {
+            // Update the view date
+            _ViewDate = _ViewDate.AddDays(1);
+
+            // Reload the UI.
+            ReloadUI();
+        }
+        #endregion
+
+        #region planner
+        #endregion
+    }
+}
---
+++ PlannerWeeklyFragment.cs
@@ -0,0 +1,77 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Front_End.Models;
+using Front_End.Backend;
+using Front_End.Exceptions;
+using SQLite;
+using Front_End.Database;
+
+namespace Front_End
+{
+    [Activity(Label = "Activity Tracker")]
+	public class PlannerWeeklyFragment : PlannerFragment
+	{
+        // TODO Swipe to move forward and backward a week at a time.
+        // TODO Update the GetActivityLogs to accept a date range which can be controlled from either Weekly or Daily activities.
+		public override void OnCreate(Bundle bundle)
+		{
+            base.OnCreate(bundle);
+
+            if(_ViewDate == default(DateTime))
+            {
+                DateTime currentTime = DateTime.Now;
+
+                // If the current view date is not set, set it to the current day.
+                _ViewDate = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, 0, 0, 0);
+            }
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Return the layout to use in this fragment.
+            return inflater.Inflate(Resource.Layout.PlannerWeekly, container, false);
+        }
+
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            base.OnActivityCreated(savedInstanceState);
+
+            // The view has been created, assign the event handlers.
+            // Assign the next and previous onclick events.
+            View.FindViewById<ImageButton>(Resource.Id.ibNextDate).Click += NextDay_OnClick;
+            View.FindViewById<ImageButton>(Resource.Id.ibPrevDate).Click += PreviousDay_OnClick;
+
+            // Add the Drag event listener to the dropzones.
+            View.FindViewById<RelativeLayout>(Resource.Id.rlSunDropzone).Drag += DropZone_Drag;
+            View.FindViewById<RelativeLayout>(Resource.Id.rlMonDropzone).Drag += DropZone_Drag;
+            View.FindViewById<RelativeLayout>(Resource.Id.rlTueDropzone).Drag += DropZone_Drag;
+            View.FindViewById<RelativeLayout>(Resource.Id.rlWedDropzone).Drag += DropZone_Drag;
+            View.FindViewById<RelativeLayout>(Resource.Id.rlThuDropzone).Drag += DropZone_Drag;
+            View.FindViewById<RelativeLayout>(Resource.Id.rlFriDropzone).Drag += DropZone_Drag;
+            View.FindViewById<RelativeLayout>(Resource.Id.rlSatDropzone).Drag += DropZone_Drag;
+        }
+
+        public override void OnResume()
+        {
+            base.OnResume();
+
+            // Reload the planner when the activity is resumed.
+            ReloadUI();
+
+            TimesDatabase timeDb = new TimesDatabase();
+
+            if (timeDb.FindLatest() != DateTime.Now.Day)
+            {
+                new Notification();
+            }
+        }
+    }
+}
